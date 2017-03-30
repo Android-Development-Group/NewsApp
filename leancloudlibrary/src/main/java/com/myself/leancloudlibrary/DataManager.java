46a887f46d75c73523990eedc65f2d8eb4961164
Jusenr <zhouxingrong@putao.com>
2017-03-30 15:11:21 +0800

    leancloud  分离

---
+++ leancloudlibrary/src/main/java/com/myself/leancloudlibrary/DataManager.java
@@ -0,0 +1,13 @@
+package com.myself.leancloudlibrary;
+
+/**
+ * Description: LeanCloud数据管理
+ * Copyright  : Copyright (c) 2017
+ * Email      : jusenr@163.com
+ * Company    : 葡萄科技
+ * Author     : Jusenr
+ * Date       : 2017/3/30 15:08.
+ */
+
+public class DataManager {
+}
